3aed8a8bd68bf3867ec81b7dbdd3004f6aecbba6
ricardohirata <ricardohirata@ymail.com>
2021-08-01 21:08:08 -0300

    Initial commit

---
+++ src/main/java/intro/Medidas.java
@@ -0,0 +1,27 @@
+// 1 - Pacote
+package intro;
+
+// 2 - Referência as bibliotecas
+
+// 3 - Classes
+public class Medidas {
+    // 3.1 - Atributos - Características
+
+    // 3.2 - Métodos e funções
+    public static void main(String[] args){
+        System.out.println("Minha primeira mensagem!!!");
+        System.out.println("Digite SOUT e dar enter que aparece System.out.println!!!");
+
+        //Calcular a área - Exemplo: o tamanho do tapete ou do piso
+        int largura=4;
+        int comprimento=3;
+        int resultado;
+
+        // largura = 4;
+        // comprimento = 3;
+        resultado = largura * comprimento;
+        System.out.printf("A área com largura %dm e com comprimento %dm = %dm².", largura, comprimento, resultado);
+
+    }
+
+}
