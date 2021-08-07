23e7b60b4751b83e858940e58df1c7ed5afc5c3d
ricardohirata <ricardohirata@ymail.com>
2021-08-07 12:00:53 -0300

    Crie os metodos calcularAreaModoCurto e calcularAreaModoLongo. O método main chama os dois metodos citados acima.

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -9,19 +9,28 @@ public class Medidas {
 
     // 3.2 - Métodos e funções
     public static void main(String[] args){
-        System.out.println("Minha primeira mensagem!!!");
         System.out.println("Digite SOUT e dar enter que aparece System.out.println!!!");
+        calcularAreaModoCurto();
+        calcularAreaModoLongo();
+    }
 
+    public static void calcularAreaModoCurto(){
         //Calcular a área - Exemplo: o tamanho do tapete ou do piso
-        int largura=4;
-        int comprimento=3;
+        int largura=4, comprimento=3;
+
+        System.out.printf("\nModo Curto!!!\nPara a largura de %dm e o comprimento de %dm a area e de %dm².", largura, comprimento, largura * comprimento);
+
+    }
+
+    public static void calcularAreaModoLongo(){
+        int largura;
+        int comprimento;
         int resultado;
 
-        // largura = 4;
-        // comprimento = 3;
+        largura = 5;
+        comprimento = 6;
         resultado = largura * comprimento;
-        System.out.printf("A área com largura %dm e com comprimento %dm = %dm².", largura, comprimento, resultado);
-
+        System.out.printf("\n\nModo Extenso!!!\nPara a largura de %dm e o comprimento de %dm a area e de %dm².", largura, comprimento, resultado);
     }
 
 }
